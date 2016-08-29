140d3952f1e2bb530c919d57a6769e38c7db4f97
Bang Nguyen <bangnv77@yahoo.com>
2016-08-28 18:43:26 -0700

    add the functionality to add new answers on new-answer component on question.hbs

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewAnswer: false,
+  actions: {
+    saveAnswer1() {
+      var params = {
+        author: this.get('author'),
+        body: this.get('body'),
+        question: this.get('question')
+      };
+      this.set('addNewAnswer', false);
+      this.sendAction('saveAnswer2', params);
+    },
+    showAnswerForm() {
+      this.set('addNewAnswer', true);
+    }
+  }
 });
