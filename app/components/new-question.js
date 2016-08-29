c123bc31d8e258416acdabf68554fc664ae61be8
Bang Nguyen <bangnv77@yahoo.com>
2016-08-28 18:30:55 -0700

    add the functionality to add new questions on new-question component on index.hbs

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+    save() {
+      var params = {
+        author: this.get('author'),
+        body: this.get('body'),
+        note: this.get('note')
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion', params);
+    }
+  }
 });
