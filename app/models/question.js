d12e5bfa7ba056f45e2dff766c5089a9abe520dd
Bang Nguyen <bangnv77@yahoo.com>
2016-08-28 17:56:02 -0700

    create content.json to seed the database with questions and answers

--- app/models/question.js
+++ app/models/question.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   body: DS.attr(),
-  notes: DS.attr(),
+  note: DS.attr(),
   author: DS.attr(),
   answers: DS.hasMany('answer', { async: true})
 });
