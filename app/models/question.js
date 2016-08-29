641486e0bf9d054952570d3913db1c2cf17d8769
Bang Nguyen <bangnv77@yahoo.com>
2016-08-28 17:46:30 -0700

    set up the models for questions and answers

--- app/models/question.js
+++ app/models/question.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  body: DS.attr(),
+  notes: DS.attr(),
+  author: DS.attr(),
+  answers: DS.hasMany('answer', { async: true})
 });
